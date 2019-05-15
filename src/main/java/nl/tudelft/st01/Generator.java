b863feaed7175729214e18491dfdd5611d0babb0
Mark Bekooy <m.j.bekooy@student.tudelft.nl>
2019-05-16 00:08:45 +0200

    Handle GROUP BY clauses in SQL queries
    Unfortunately Checkstyle is throwing warnings for duplicate words, but those must be duplicate as the word "count" should be set twice. I also couldn't get suppressing Checkstyle warnings to work...
    
    Furthermore, the committed code is currently one massive function, but it will likely be split up later in smaller methods.

--- src/main/java/nl/tudelft/st01/Generator.java
+++ src/main/java/nl/tudelft/st01/Generator.java
@@ -62,7 +62,7 @@ public final class Generator {
      */
     public static void main(String[] args) {
 
-        String query = "SELECT * FROM Table1 WHERE a1=111 AND (a2>222 OR a3<>333)";
+        String query = "SELECT director FROM Movies GROUP BY director HAVING count(*)>1";
         Set<String> result = generateRules(query);
 
         System.out.println("Result: " + result.toString());
--- src/main/java/nl/tudelft/st01/RuleGeneratorSelectVisitor.java
+++ src/main/java/nl/tudelft/st01/RuleGeneratorSelectVisitor.java
@@ -1,7 +1,12 @@
 package nl.tudelft.st01;
 
+import net.sf.jsqlparser.expression.DoubleValue;
 import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.Function;
+import net.sf.jsqlparser.expression.operators.relational.ExpressionList;
+import net.sf.jsqlparser.expression.operators.relational.GreaterThan;
 import net.sf.jsqlparser.statement.select.PlainSelect;
+import net.sf.jsqlparser.statement.select.SelectExpressionItem;
 import net.sf.jsqlparser.statement.select.SelectVisitorAdapter;
 
 import java.util.ArrayList;
@@ -39,6 +44,52 @@ public class RuleGeneratorSelectVisitor extends SelectVisitorAdapter {
 
                 output.add(plainSelectOut);
             }
+        }
+
+        if (plainSelect.getGroupBy() != null) {
+            // First statement for branch coverage
+            PlainSelect plainSelectOut = new PlainSelect();
+
+            GreaterThan gt = new GreaterThan();
+            Function count = new Function();
+            count.setName("count");
+            count.setAllColumns(true);
+
+            gt.setLeftExpression(count);
+            gt.setRightExpression(new DoubleValue("1"));
+
+            plainSelectOut.setSelectItems(plainSelect.getSelectItems());
+            plainSelectOut.setFromItem(plainSelect.getFromItem());
+            plainSelectOut.setGroupByElement(plainSelect.getGroupBy());
+            plainSelectOut.setHaving(gt);
+
+            output.add(plainSelectOut);
+
+            // Second statement for branch coverage
+            PlainSelect plainSelectOut2 = new PlainSelect();
+
+            GreaterThan gt2 = new GreaterThan();
+            Function count2 = new Function();
+            List groupByValue = new ArrayList<Expression>();
+            groupByValue.add(plainSelect.getGroupBy().getGroupByExpressions().get(0));
+            count2.setName("count");
+            ExpressionList parameters = new ExpressionList();
+            parameters.setExpressions(groupByValue);
+            count2.setParameters(parameters);
+            count2.setDistinct(true);
+
+            gt2.setLeftExpression(count2);
+            gt2.setRightExpression(new DoubleValue("1"));
+
+            SelectExpressionItem selectItem = new SelectExpressionItem(count);
+            selectItem.setExpression(count);
+
+            plainSelectOut2.addSelectItems(selectItem);
+            plainSelectOut2.setFromItem(plainSelect.getFromItem());
+            plainSelectOut2.setGroupByElement(plainSelect.getGroupBy());
+            plainSelectOut2.setHaving(gt2);
+
+            output.add(plainSelectOut2);
         }
 
         output = null;
