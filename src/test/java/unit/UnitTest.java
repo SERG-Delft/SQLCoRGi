cc8595d93ffe002c1e7ed647be70ca1daa4c0b63
drdenouden <d.r.denouden@student.tudelft.nl>
2019-05-24 11:48:40 +0200

    Add unit package with sample unit test class

---
+++ src/test/java/unit/UnitTest.java
@@ -0,0 +1,19 @@
+package unit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Sample unit test class.
+ */
+public class UnitTest {
+
+    /**
+     * Sample test with AssertJ.
+     */
+    @Test
+    public void testJoinOnConditionFromSingleTableLeftNullable() {
+        assertThat(true).isTrue();
+    }
+}
