2a174eb281f6280c5d79e450a7c638f87fa7af9f
drdenouden <d.r.denouden@student.tudelft.nl>
2019-05-24 15:32:36 +0200

    Add explanation for suppression of Checkstyle warning

--- src/test/java/nl/tudelft/st01/functional/CombinedTest.java
+++ src/test/java/nl/tudelft/st01/functional/CombinedTest.java
@@ -7,6 +7,8 @@ import static nl.tudelft.st01.functional.AssertUtils.verify;
 /**
  * This class tests if the coverage targets for complex queries (with joins, conditionals, aggregation functions, etc.)
  * are generated correctly.
+ *
+ * Suppresses the checkstyle multipleStringLiterals violation, because some output sets have queries in common.
  */
 @SuppressWarnings("checkstyle:multipleStringLiterals")
 public class CombinedTest {
