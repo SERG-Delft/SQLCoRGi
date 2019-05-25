9c1dc9973b2ec4d8065bfdf73feafbf96b31b876
Rolf Piepenbrink <r.h.piepenbrink@student.tudelft.nl>
2019-05-25 14:42:08 +0200

    Add visitor for null column exclusion

---
+++ src/main/java/nl/tudelft/st01/NullColumnExclusionVisitor.java
@@ -0,0 +1,80 @@
+package nl.tudelft.st01;
+
+import net.sf.jsqlparser.expression.ExpressionVisitorAdapter;
+import net.sf.jsqlparser.expression.operators.conditional.AndExpression;
+import net.sf.jsqlparser.expression.operators.conditional.OrExpression;
+import net.sf.jsqlparser.expression.operators.relational.EqualsTo;
+import net.sf.jsqlparser.expression.operators.relational.GreaterThan;
+import net.sf.jsqlparser.expression.operators.relational.GreaterThanEquals;
+import net.sf.jsqlparser.expression.operators.relational.IsNullExpression;
+import net.sf.jsqlparser.expression.operators.relational.MinorThan;
+import net.sf.jsqlparser.expression.operators.relational.MinorThanEquals;
+import net.sf.jsqlparser.expression.operators.relational.NotEqualsTo;
+import net.sf.jsqlparser.schema.Column;
+
+import java.util.List;
+
+public class NullColumnExclusionVisitor extends ExpressionVisitorAdapter {
+    private List<Column> columns;
+
+    @Override
+    public void visit(AndExpression andExpression) {
+        
+    }
+
+    @Override
+    public void visit(OrExpression orExpression) {
+
+    }
+
+    @Override
+    public void visit(EqualsTo equalsTo) {
+
+    }
+
+    @Override
+    public void visit(GreaterThan greaterThan) {
+
+    }
+
+    @Override
+    public void visit(GreaterThanEquals greaterThanEquals) {
+
+    }
+
+    @Override
+    public void visit(IsNullExpression isNullExpression) {
+
+    }
+
+    @Override
+    public void visit(MinorThan minorThan) {
+
+    }
+
+    @Override
+    public void visit(MinorThanEquals minorThanEquals) {
+
+    }
+
+    @Override
+    public void visit(NotEqualsTo notEqualsTo) {
+
+    }
+
+    @Override
+    public void visit(Column column) {
+
+    }
+
+    public void setColumns(List<Column> columns) {
+        this.columns = columns;
+    }
+
+    private void contains(Column column) {
+
+    }
+
+
+
+}
