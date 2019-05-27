5d75145943b0308cf1172fede912909f8f883394
Dylan Franken <d.t.franken@student.tudelft.nl>
2019-05-27 14:32:42 +0200

    Hoist methods of interest to top

--- src/main/java/nl/tudelft/st01/visitors/join/RuleGeneratorOnExpressionVisitor.java
+++ src/main/java/nl/tudelft/st01/visitors/join/RuleGeneratorOnExpressionVisitor.java
@@ -20,11 +20,62 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * This Visitor allows for extracting the column used in the given expression.
+ * This visitor allows for extracting the column used in the given expression.
  */
 public class RuleGeneratorOnExpressionVisitor extends ExpressionVisitorAdapter {
 
-    private Map<String, List<Expression>> output = new HashMap();
+    private Map<String, List<Expression>> output = new HashMap<>();
+
+    /**
+     * Retrieves the columns and values used in the on expressions.
+     * @param binaryExpression binary expression.
+     */
+    private void getColumnsOnCondition(BinaryExpression binaryExpression) {
+        binaryExpression.getLeftExpression().accept(this);
+        binaryExpression.getRightExpression().accept(this);
+    }
+
+    /**
+     * Stores each column corresponding to its table.
+     * @param e The column that should be added.
+     */
+    private void updateColumnList(Column e) {
+        String table = e.getTable().toString().toLowerCase();
+
+        if (!output.containsKey(table)) {
+            List<Expression> list = new ArrayList<>();
+            list.add(e);
+            output.put(table, list);
+        } else if (!contains(output.get(table), e)) {
+            output.get(table).add(e);
+        }
+    }
+
+
+
+    public void setOutput(Map map) {
+        this.output = map;
+    }
+
+    /**
+     * Ensures that the list of columns used in the on expression contains no duplicate column names.
+     * NOTE: Suppose the column name "id" in "Movies" and the column name is unique.
+     * Yet, the function considers Movies.id to be different from id.
+     * @param list list
+     * @param expression e
+     * @return boolean
+     */
+    private static boolean contains(List<Expression> list, Expression expression) {
+        if (list == null) {
+            return false;
+        }
+        for (Expression e : list) {
+            if (e.toString().toLowerCase().equals(expression.toString().toLowerCase())) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     @Override
     public void visit(AndExpression andExpression) {
@@ -74,60 +125,6 @@ public class RuleGeneratorOnExpressionVisitor extends ExpressionVisitorAdapter {
     @Override
     public void visit(Column column) {
         updateColumnList(column);
-    }
-
-    /**
-     * Retrieves the columns and values used in the on expressions.
-     * @param binaryExpression binary expression.
-     */
-    private void getColumnsOnCondition(BinaryExpression binaryExpression) {
-        Expression left = binaryExpression.getLeftExpression();
-        Expression right = binaryExpression.getRightExpression();
-
-        left.accept(this);
-        right.accept(this);
-    }
-
-    /**
-     * Stores each column corresponding to its table.
-     * @param e The column that should be added.
-     */
-    private void updateColumnList(Column e) {
-        String table = e.getTable().toString().toLowerCase();
-
-        if (!output.containsKey(table)) {
-            List<Expression> list = new ArrayList<>();
-            list.add(e);
-            output.put(table, list);
-        } else if (!contains(output.get(table), e)) {
-            output.get(table).add(e);
-        }
-    }
-
-
-
-    public void setOutput(Map map) {
-        this.output = map;
-    }
-
-    /**
-     * Ensures that the list of columns used in the on expression contains no duplicate column names.
-     * NOTE: Suppose the column name "id" in "Movies" and the column name is unique.
-     * Yet, the function considers Movies.id to be different from id.
-     * @param list list
-     * @param expression e
-     * @return boolean
-     */
-    private static boolean contains(List<Expression> list, Expression expression) {
-        if (list == null) {
-            return false;
-        }
-        for (Expression e : list) {
-            if (e.toString().toLowerCase().equals(expression.toString().toLowerCase())) {
-                return true;
-            }
-        }
-        return false;
     }
 
 }
