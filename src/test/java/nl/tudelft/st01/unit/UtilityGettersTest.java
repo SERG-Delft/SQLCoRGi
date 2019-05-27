2d660ad4e6808c8242fd60f390a9b4bd9bf9dc92
Mark Bekooy <m.j.bekooy@student.tudelft.nl>
2019-05-27 20:27:40 +0200

    Rename UnitTest file to UtilityGettersTest as it only contained those tests and the previous name was only an example

--- src/test/java/nl/tudelft/st01/unit/UtilityGettersTest.java
+++ src/test/java/nl/tudelft/st01/unit/UtilityGettersTest.java
@@ -10,15 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * Sample nl.tudelft.st01.unit test class.
  */
-public class UnitTest {
-
-    /**
-     * Sample test with AssertJ.
-     */
-    @Test
-    public void sampleUnitTest() {
-        assertThat(true).isTrue();
-    }
+public class UtilityGettersTest {
 
     /**
      * Unit test case for UtilityGetters getCountAllColumns function.
