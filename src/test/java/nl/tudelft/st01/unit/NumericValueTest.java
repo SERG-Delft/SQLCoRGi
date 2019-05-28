df02a937c6f607675dc9516001d9e0180c05833c
Timon Bestebreur <t.a.a.bestebreur@student.tudelft.nl>
2019-05-28 13:41:35 +0200

    Add parameterized test for NumericValue Double

--- src/test/java/nl/tudelft/st01/unit/NumericValueTest.java
+++ src/test/java/nl/tudelft/st01/unit/NumericValueTest.java
@@ -3,6 +3,8 @@ package nl.tudelft.st01.unit;
 import net.sf.jsqlparser.expression.DoubleValue;
 import nl.tudelft.st01.query.NumericDoubleValue;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,12 +14,34 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class NumericValueTest {
 
     /**
-     * Sample test with AssertJ.
+     * Test for NumericDoubleValue.
      */
     @Test
     public void correctSuperclass() {
         NumericDoubleValue n = new NumericDoubleValue("123");
 
         assertThat(n instanceof DoubleValue).isTrue();
+    }
+
+    /**
+     * Test for NumericDoubleValue.
+     */
+    @Test
+    public void getTest() {
+        NumericDoubleValue n = new NumericDoubleValue("123");
+
+        assertThat(n.getValue()).isEqualTo(123);
+    }
+
+    /**
+     * Test for NumericDoubleValue.
+     */
+    @ParameterizedTest(name = "[{index}] number to add: {0}")
+    @CsvSource({"1", "-1", "0", "10"})
+    public void addTest(int input) {
+        NumericDoubleValue oldValue = new NumericDoubleValue("123");
+        NumericDoubleValue newValue = (NumericDoubleValue) oldValue.add(input);
+
+        assertThat(newValue.getValue()).isEqualTo(123 + input);
     }
 }
