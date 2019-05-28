19609a298604a606e9df58745f588ba9524b7a4c
Timon Bestebreur <t.a.a.bestebreur@student.tudelft.nl>
2019-05-28 14:03:41 +0200

    Add test for JoinWhereItem

---
+++ src/test/java/nl/tudelft/st01/unit/JoinWhereItemTest.java
@@ -0,0 +1,53 @@
+package nl.tudelft.st01.unit;
+
+import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.operators.relational.GreaterThan;
+import net.sf.jsqlparser.statement.select.Join;
+import nl.tudelft.st01.query.JoinWhereItem;
+import nl.tudelft.st01.query.NumericDoubleValue;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Sample nl.tudelft.st01.unit test class.
+ */
+public class JoinWhereItemTest {
+    private static final Join JOIN = new Join();
+
+    //    For now it does not matter what is in the expression, just that it is an expression object
+    private static final Expression WHERE = new GreaterThan();
+
+    /**
+     * Test for JoinWhereItem.
+     */
+    @Test
+    public void constructorNotNull() {
+        JoinWhereItem joinWhereItem = new JoinWhereItem(JOIN, WHERE);
+
+        assertThat(joinWhereItem).isNotNull();
+    }
+
+    /**
+     * Test for JoinWhereItem.
+     */
+    @Test
+    public void getJoinTest() {
+        JoinWhereItem joinWhereItem = new JoinWhereItem(JOIN, WHERE);
+
+        assertThat(joinWhereItem.getJoin()).isEqualTo(JOIN);
+    }
+
+    /**
+     * Test for JoinWhereItem.
+     */
+    @Test
+    public void getJoinWhereTest() {
+        JoinWhereItem joinWhereItem = new JoinWhereItem(JOIN, WHERE);
+
+        assertThat(joinWhereItem.getJoinWhere()).isEqualTo(WHERE);
+    }
+
+}
