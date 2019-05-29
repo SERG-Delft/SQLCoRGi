2ebf37f2e07106b7534bda6406cae1f8d6bbf5f3
drdenouden <d.r.denouden@student.tudelft.nl>
2019-05-29 10:38:48 +0200

    Fix various checkstyle violations

--- src/test/java/nl/tudelft/st01/unit/visitors/SelectStatementVisitorTest.java
+++ src/test/java/nl/tudelft/st01/unit/visitors/SelectStatementVisitorTest.java
@@ -16,20 +16,26 @@ public class SelectStatementVisitorTest {
     private static final String EXCEPTION_MESSAGE = "A SelectStatementVisitor requires an empty, non-null set to which "
             + "it can output generated rules.";
 
+    /**
+     * Assert that the proper exception is thrown when the constructor is provided a null argument.
+     */
     @Test
     public void testConstructorWithNullSetThrowsException() {
         assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(
-                () -> new SelectStatementVisitor(null)
+            () -> new SelectStatementVisitor(null)
         );
     }
 
+    /**
+     * Assert that the proper exception is thrown when the constructor is provided a non-empty set.
+     */
     @Test
     public void testConstructorWithNonEmptySetThrowsException() {
         Set<String> nonEmptySet = new HashSet<>();
         nonEmptySet.add("");
 
         assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(
-                () -> new SelectStatementVisitor(nonEmptySet)
+            () -> new SelectStatementVisitor(nonEmptySet)
         ).withMessage(EXCEPTION_MESSAGE);
     }
 }
