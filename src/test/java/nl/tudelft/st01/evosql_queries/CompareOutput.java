23787740a8bbeac111606a7274a12a997173ae0e
Timon Bestebreur <t.a.a.bestebreur@student.tudelft.nl>
2019-06-04 15:23:20 +0200

    Prepare CompareOutput class for handling the files

--- src/test/java/nl/tudelft/st01/evosql_queries/CompareOutput.java
+++ src/test/java/nl/tudelft/st01/evosql_queries/CompareOutput.java
@@ -7,18 +7,28 @@ import java.io.FileReader;
 import java.io.File;
 import java.util.Scanner;
 import java.util.Set;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.net.URI;
+import java.net.URL;
 
 public class CompareOutput {
-    private static String path = "C:/Users/Timon Bestebreur/Dropbox/Opleiding/Technische Informatica/Y2/Context Project/coverage_generator/src/test/java/nl/tudelft/st01/evosql_queries/suitecrm_input.txt";
-    static Scanner sc;
-    static File f;
+
 
     public static void main(String[] args) {
-        System.out.println("Latsgooo");
+        String basePath = "C:/Users/Timon Bestebreur/Dropbox/Opleiding/Technische Informatica/Y2/Context Project/coverage_generator/src/test/java/nl/tudelft/st01/evosql_queries/query_resources/";
+
+        String suitecrm_input = "suitecrm_input.txt";
+        String suitecrm_output = "suitecrm_output.json";
+        String espocrm_input = "espocrm_input.txt";
+        String espocrm_output = "espocrm_output.json";
+        String erpnext_input = "erpnext_input.txt";
+        String erpnext_output = "erpnext_output.json";
+
+        Scanner sc = null;
         try {
-            f = new File(path);
-            sc = new Scanner(new FileReader(path));
-        } catch (FileNotFoundException e) {
+            sc = new Scanner(new FileReader(basePath + suitecrm_input));
+        } catch (Exception e) {
             e.printStackTrace();
         }
         int i = 2;
