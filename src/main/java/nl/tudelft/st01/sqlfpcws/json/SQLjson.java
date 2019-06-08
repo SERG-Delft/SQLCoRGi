1e9c3daccdbd9afab570d796ebc306c7656cd41d
drdenouden <d.r.denouden@student.tudelft.nl>
2019-06-09 01:12:04 +0200

    Add GSON serialization to SQLjson

--- src/main/java/nl/tudelft/st01/sqlfpcws/json/SQLjson.java
+++ src/main/java/nl/tudelft/st01/sqlfpcws/json/SQLjson.java
@@ -3,8 +3,13 @@ package nl.tudelft.st01.sqlfpcws.json;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class SQLjson {
 
+    @SerializedName("entries")
+    @Expose
     private List<SQLRules> entries = new ArrayList<>();
 
     public void addEntry(SQLRules rules) {
