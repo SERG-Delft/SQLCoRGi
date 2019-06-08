2faabddc64d83ca6668d39d72f48f677fd828ad2
drdenouden <d.r.denouden@student.tudelft.nl>
2019-06-09 01:13:07 +0200

    Add GSON serialization to SQLRules

--- src/main/java/nl/tudelft/st01/sqlfpcws/json/SQLRules.java
+++ src/main/java/nl/tudelft/st01/sqlfpcws/json/SQLRules.java
@@ -3,10 +3,17 @@ package nl.tudelft.st01.sqlfpcws.json;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class SQLRules {
 
+    @SerializedName("queryNo")
+    @Expose
     private Integer queryNo;
 
+    @SerializedName("pathList")
+    @Expose
     private List<String> pathList = new ArrayList<>();
 
     public SQLRules(Integer queryNo) {
