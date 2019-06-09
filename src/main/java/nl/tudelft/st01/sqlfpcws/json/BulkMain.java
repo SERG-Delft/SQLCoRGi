38219a79db076b5c9adc1cda7c1ba18822e0a719
drdenouden <d.r.denouden@student.tudelft.nl>
2019-06-09 20:44:24 +0200

    Add constructor to BulkRuleGenerator

--- src/main/java/nl/tudelft/st01/sqlfpcws/json/BulkMain.java
+++ src/main/java/nl/tudelft/st01/sqlfpcws/json/BulkMain.java
@@ -3,28 +3,18 @@ package nl.tudelft.st01.sqlfpcws.json;
 import org.dom4j.Document;
 import org.dom4j.io.SAXReader;
 
-import java.io.InputStreamReader;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
-
 public final class BulkMain {
 
     private static final String DATABASE = "suitecrm";
 
-    private static final String SQL_INPUT_PATH = "/database/input_queries/" + DATABASE + ".sql";
-    private static final String XML_SCHEMA_PATH = "/database/schema_xml/" + DATABASE + ".xml";
-    private static final String JSON_OUTPUT_PATH = "/database/output_json/" + DATABASE + ".json";
+    private static final String SQL_INPUT_PATH = ".src/main/resources/database/input_queries" + DATABASE + ".sql";
+    private static final String XML_SCHEMA_PATH = ".src/main/resources/database/schema_xml" + DATABASE + ".xml";
+    private static final String JSON_OUTPUT_PATH = ".src/main/resources/database/output_json" + DATABASE + ".json";
 
     private static final int RULE_GENERATOR_INTERVAL = 1000;
 
-    public static void main(String[] args) throws Exception{
-        SAXReader reader = new SAXReader();
+    public static void main(String[] args) {
 
-        InputStreamReader isr = new InputStreamReader(BulkMain.class.getResourceAsStream(XML_SCHEMA_PATH), UTF_8);
-
-        Document schema = reader.read(isr);
-
-        System.out.println(schema.getStringValue());
 
     }
 
--- src/main/java/nl/tudelft/st01/sqlfpcws/json/BulkRuleGenerator.java
+++ src/main/java/nl/tudelft/st01/sqlfpcws/json/BulkRuleGenerator.java
@@ -1,4 +1,77 @@
 package nl.tudelft.st01.sqlfpcws.json;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
+import org.dom4j.io.SAXReader;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class BulkRuleGenerator {
+
+    private List<String> queriesToParse;
+    private Document schema;
+
+    private FileWriter jsonWriter;
+    private Gson gson;
+
+    private SQLjson sqlJson;
+    private int queryNo;
+
+
+    public BulkRuleGenerator(String sqlInputPath, String xmlSchemaPath, String jsonOutputPath) {
+        setUpQueriesToParse(sqlInputPath);
+        setUpSchemaDocument(xmlSchemaPath);
+        setUpOutputJsonWriter(jsonOutputPath);
+
+        this.queryNo = 0;
+        this.sqlJson = new SQLjson();
+    }
+
+    private void setUpQueriesToParse(String sqlInputPath) {
+        try (Stream<String> stream = Files.lines(Paths.get(sqlInputPath))) {
+            queriesToParse = stream
+                    .filter(query -> !query.isEmpty())
+                    .map(String::toLowerCase)
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            System.err.println("Could not open schema file: " + e.getMessage());
+            queriesToParse = new ArrayList<>();
+        }
+    }
+
+    private void setUpSchemaDocument(String xmlSchemaPath) {
+        try {
+            schema = new SAXReader().read(new File(xmlSchemaPath));
+        } catch (DocumentException e) {
+            System.err.println("Schema could not be parsed: " + e.getMessage());
+            schema = DocumentHelper.createDocument();
+        }
+    }
+
+    private void setUpOutputJsonWriter(String jsonOutputPath) {
+        try {
+            jsonWriter = new FileWriter(jsonOutputPath);
+        } catch (IOException e) {
+            System.err.println("Output file could not be opened: " + e.getMessage());
+        }
+
+        gson = new GsonBuilder()
+                .disableHtmlEscaping()
+                .setPrettyPrinting()
+                .create();
+    }
+
+
+
 }
