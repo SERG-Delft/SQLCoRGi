45682c1e2fb7bfd6e90faa5bbb3ada3ab93f6c5c
drdenouden <d.r.denouden@student.tudelft.nl>
2019-06-09 02:02:25 +0200

    Add method to get the coverage targets from SQLFpc

--- src/main/java/nl/tudelft/st01/sqlfpcws/json/SQLFpcWS.java
+++ src/main/java/nl/tudelft/st01/sqlfpcws/json/SQLFpcWS.java
@@ -1,5 +1,6 @@
 package nl.tudelft.st01.sqlfpcws.json;
 
+import es.uniovi.lsi.in2test.sqlfpcws.SQLFpcWSSoapProxy;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Element;
@@ -8,6 +9,7 @@ import org.dom4j.io.SAXReader;
 
 import java.io.StringReader;
 
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,6 +17,23 @@ public final class SQLFpcWS {
 
     private static final String ERROR_XPATH = "/sqlfpc/error";
     private static final String SQL_TARGET_XPATH = "/sqlfpc/fpcrules/fpcrule/sql";
+
+    public List<String> getCoverageTargets(String sqlQuery, String schema, String options) {
+        SQLFpcWSSoapProxy proxy = new SQLFpcWSSoapProxy();
+        List<String> result;
+
+        String xmlResponseString;
+        try {
+            xmlResponseString = proxy.getRules(sqlQuery, schema, options);
+        } catch (RemoteException e) {
+            System.err.println("Server error: " + e.getMessage());
+            return new ArrayList<>();
+        }
+
+        result = extractSQLTargetsFromXMLResponse(xmlResponseString);
+
+        return result;
+    }
 
     private List<String> extractSQLTargetsFromXMLResponse(String xmlResponseString) {
         ArrayList<String> result = new ArrayList<>();
