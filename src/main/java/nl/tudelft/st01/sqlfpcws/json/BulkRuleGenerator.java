7c5a00b6c9c121627ffbedd89ab3720f63c307fb
drdenouden <d.r.denouden@student.tudelft.nl>
2019-06-09 21:01:08 +0200

    Add methods/skeleton to bulk generate rules

--- src/main/java/nl/tudelft/st01/sqlfpcws/json/BulkRuleGenerator.java
+++ src/main/java/nl/tudelft/st01/sqlfpcws/json/BulkRuleGenerator.java
@@ -2,9 +2,15 @@ package nl.tudelft.st01.sqlfpcws.json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.util.TablesNamesFinder;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
 
 import java.io.File;
@@ -13,6 +19,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -72,6 +79,48 @@ public class BulkRuleGenerator {
                 .create();
     }
 
+    public void generate() {
+        // Loop over each query
+            // Filter schema
+            // Send to SQLFpc
+            // Add response to output
 
+        // Save JSON file
+    }
+
+    private List<String> getInvolvedTableFromQuery(String query) {
+        List<String> tableNames = Collections.emptyList();
+
+        Statement statement;
+        try {
+            statement = CCJSqlParserUtil.parse(query);
+        } catch (JSQLParserException e) {
+            System.err.println("Input query could not be parsed: " + e.getMessage());
+            return tableNames;
+        }
+
+        tableNames = new TablesNamesFinder().getTableList(statement);
+        tableNames.replaceAll(e -> e.replace("\"", ""));
+
+
+        return tableNames;
+    }
+
+    @SuppressWarnings("unchecked")
+    private Document filterSchema(String query) {
+        List<String> tableNames = getInvolvedTableFromQuery(query);
+        Document filteredSchema = (Document)schema.clone();
+
+        List<Node> tables = (List<Node>)filteredSchema.selectNodes("/schema/table");
+        for (Node table : tables) {
+            String tableNameXML = ((Element) table).attribute("name").getValue().toLowerCase();
+
+            if(!tableNames.contains(tableNameXML)) {
+                table.detach();
+            }
+        }
+
+        return filteredSchema;
+    }
 
 }
