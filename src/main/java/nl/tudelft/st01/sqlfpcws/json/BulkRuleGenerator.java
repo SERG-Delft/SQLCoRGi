a0e36f70993bf97396395f360b111d493d06ecb5
drdenouden <d.r.denouden@student.tudelft.nl>
2019-06-11 12:45:17 +0200

    Remove unused imports

--- src/main/java/nl/tudelft/st01/sqlfpcws/json/BulkRuleGenerator.java
+++ src/main/java/nl/tudelft/st01/sqlfpcws/json/BulkRuleGenerator.java
@@ -19,11 +19,8 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.OpenOption;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
