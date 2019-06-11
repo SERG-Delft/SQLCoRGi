2d6b60ba9a67cdc3e23b72702b358b8c81b00981
Rolf Piepenbrink <r.h.piepenbrink@student.tudelft.nl>
2019-06-11 13:43:37 +0200

    Add tests for bug fix

--- src/test/java/nl/tudelft/st01/functional/NetedJoinsTest.java
+++ src/test/java/nl/tudelft/st01/functional/NetedJoinsTest.java
@@ -40,11 +40,113 @@ public class NetedJoinsTest {
                         + "WHERE (b.id IS NULL) AND (c.id IS NULL)");
     }
 
+    /**
+     * This test verifies whether the correct columns are excluded depending on the join configuration. In case of an
+     * INNER JOIN in the configuration, the tables used in said join must be included. Unless the tables from said join
+     * use one or more tables used in the inspected join. In that case, the columns from those tables are to be
+     * excluded. This test examines the behaviour for including columns not used in the on expression
+     * in the where expression, even though these columns' tables key is in the on expression.
+     * In this case, the outer increment relations only contain a single table.
+     */
     @Test
     public void testNestedJoinNullReductionSingleTableOINonOIRColumnIncluded() {
-//        containsAtLeast("SELECT * FROM a INNER JOIN b ON b.id = a.id INNER JOIN c ON c.id = b.id"
-//                + "WHERE a.length > 1 OR b.length > 1",
-//                );
+        containsAtLeast("SELECT * FROM a INNER JOIN b ON b.id = a.id INNER JOIN c ON c.id = b.id "
+                        + "INNER JOIN d on d.id = a.id WHERE c.length > 1 OR b.length > 1 OR a.length > 1 "
+                        + "OR d.length > 1",
+                "SELECT * FROM a LEFT JOIN b ON b.id = a.id LEFT JOIN c ON c.id = b.id INNER JOIN d ON d.id = a.id "
+                        + "WHERE ((b.id IS NULL) AND (a.id IS NOT NULL)) AND (a.length > 1 OR d.length > 1)",
+                "SELECT * FROM a LEFT JOIN b ON b.id = a.id LEFT JOIN c ON c.id = b.id INNER JOIN d ON d.id = a.id "
+                        + "WHERE ((b.id IS NULL) AND (a.id IS NULL)) AND (a.length > 1 OR d.length > 1)",
+                "SELECT * FROM a RIGHT JOIN b ON b.id = a.id INNER JOIN c ON c.id = b.id LEFT JOIN d ON d.id = a.id "
+                        + "WHERE ((a.id IS NULL) AND (b.id IS NOT NULL)) AND (c.length > 1 OR b.length > 1)",
+                "SELECT * FROM a RIGHT JOIN b ON b.id = a.id INNER JOIN c ON c.id = b.id LEFT JOIN d ON d.id = a.id "
+                        + "WHERE ((a.id IS NULL) AND (b.id IS NULL)) AND (c.length > 1 OR b.length > 1)",
+                "SELECT * FROM a INNER JOIN b ON b.id = a.id LEFT JOIN c ON c.id = b.id INNER JOIN d ON d.id = a.id "
+                        + "WHERE ((c.id IS NULL) AND (b.id IS NOT NULL)) "
+                        + "AND (b.length > 1 OR a.length > 1 OR d.length > 1)",
+                "SELECT * FROM a INNER JOIN b ON b.id = a.id LEFT JOIN c ON c.id = b.id INNER JOIN d ON d.id = a.id "
+                        + "WHERE ((c.id IS NULL) AND (b.id IS NULL)) AND (b.length > 1 OR a.length > 1 OR d.length > 1)",
+                "SELECT * FROM a RIGHT JOIN b ON b.id = a.id RIGHT JOIN c ON c.id = b.id LEFT JOIN d ON d.id = a.id "
+                        + "WHERE ((b.id IS NULL) AND (c.id IS NOT NULL)) AND (c.length > 1)",
+                "SELECT * FROM a RIGHT JOIN b ON b.id = a.id RIGHT JOIN c ON c.id = b.id LEFT JOIN d ON d.id = a.id "
+                        + "WHERE ((b.id IS NULL) AND (c.id IS NULL)) AND (c.length > 1)",
+                "SELECT * FROM a INNER JOIN b ON b.id = a.id INNER JOIN c ON c.id = b.id LEFT JOIN d ON d.id = a.id "
+                        + "WHERE ((d.id IS NULL) AND (a.id IS NOT NULL)) "
+                        + "AND (c.length > 1 OR b.length > 1 OR a.length > 1)",
+                "SELECT * FROM a INNER JOIN b ON b.id = a.id INNER JOIN c ON c.id = b.id LEFT JOIN d ON d.id = a.id "
+                        + "WHERE ((d.id IS NULL) AND (a.id IS NULL)) "
+                        + "AND (c.length > 1 OR b.length > 1 OR a.length > 1)",
+                "SELECT * FROM a LEFT JOIN b ON b.id = a.id LEFT JOIN c ON c.id = b.id RIGHT JOIN d ON d.id = a.id "
+                        + "WHERE ((a.id IS NULL) AND (d.id IS NOT NULL)) AND (d.length > 1)",
+                "SELECT * FROM a LEFT JOIN b ON b.id = a.id LEFT JOIN c ON c.id = b.id RIGHT JOIN d ON d.id = a.id "
+                        + "WHERE ((a.id IS NULL) AND (d.id IS NULL)) AND (d.length > 1)");
+    }
+
+    /**
+     * This test verifies whether the correct columns are excluded depending on the join configuration. In case of an
+     * INNER JOIN in the configuration, the tables used in said join must be included. Unless the tables from said join
+     * use one or more tables used in the inspected join. In that case, the columns from those tables are to be
+     * excluded. This test examines the behaviour for including columns not used in the on expression
+     * in the where expression, even though these columns' tables key is in the on expression.
+     * In this case, the outer increment relations contain multiple tables.
+     */
+    @Test
+    public void testNestedJoinNullReductionMultiTableOINonOIRColumnIncluded() {
+        containsAtLeast("SELECT * FROM a INNER JOIN b ON b.id = a.id INNER JOIN c ON c.id = b.id "
+                        + "INNER JOIN d on d.id = a.id WHERE c.length > 1 OR b.length > 1 OR a.length > 1 "
+                        + "OR d.length > 1",
+                "SELECT * FROM a LEFT JOIN b ON b.id = a.id LEFT JOIN c ON c.id = b.id INNER JOIN d ON d.id = a.id "
+                        + "WHERE ((b.id IS NULL) AND (a.id IS NOT NULL)) AND (a.length > 1 OR d.length > 1)",
+                "SELECT * FROM a LEFT JOIN b ON b.id = a.id LEFT JOIN c ON c.id = b.id INNER JOIN d ON d.id = a.id "
+                        + "WHERE ((b.id IS NULL) AND (a.id IS NULL)) AND (a.length > 1 OR d.length > 1)",
+                "SELECT * FROM a RIGHT JOIN b ON b.id = a.id INNER JOIN c ON c.id = b.id LEFT JOIN d ON d.id = a.id "
+                        + "WHERE ((a.id IS NULL) AND (b.id IS NOT NULL)) AND (c.length > 1 OR b.length > 1)",
+                "SELECT * FROM a RIGHT JOIN b ON b.id = a.id INNER JOIN c ON c.id = b.id LEFT JOIN d ON d.id = a.id "
+                        + "WHERE ((a.id IS NULL) AND (b.id IS NULL)) AND (c.length > 1 OR b.length > 1)",
+                "SELECT * FROM a INNER JOIN b ON b.id = a.id LEFT JOIN c ON c.id = b.id INNER JOIN d ON d.id = a.id "
+                        + "WHERE ((c.id IS NULL) AND (b.id IS NOT NULL)) "
+                        + "AND (b.length > 1 OR a.length > 1 OR d.length > 1)",
+                "SELECT * FROM a INNER JOIN b ON b.id = a.id LEFT JOIN c ON c.id = b.id INNER JOIN d ON d.id = a.id "
+                        + "WHERE ((c.id IS NULL) AND (b.id IS NULL)) AND (b.length > 1 OR a.length > 1 OR d.length > 1)",
+                "SELECT * FROM a RIGHT JOIN b ON b.id = a.id RIGHT JOIN c ON c.id = b.id LEFT JOIN d ON d.id = a.id "
+                        + "WHERE ((b.id IS NULL) AND (c.id IS NOT NULL)) AND (c.length > 1)",
+                "SELECT * FROM a RIGHT JOIN b ON b.id = a.id RIGHT JOIN c ON c.id = b.id LEFT JOIN d ON d.id = a.id "
+                        + "WHERE ((b.id IS NULL) AND (c.id IS NULL)) AND (c.length > 1)",
+                "SELECT * FROM a INNER JOIN b ON b.id = a.id INNER JOIN c ON c.id = b.id LEFT JOIN d ON d.id = a.id "
+                        + "WHERE ((d.id IS NULL) AND (a.id IS NOT NULL)) "
+                        + "AND (c.length > 1 OR b.length > 1 OR a.length > 1)",
+                "SELECT * FROM a INNER JOIN b ON b.id = a.id INNER JOIN c ON c.id = b.id LEFT JOIN d ON d.id = a.id "
+                        + "WHERE ((d.id IS NULL) AND (a.id IS NULL)) "
+                        + "AND (c.length > 1 OR b.length > 1 OR a.length > 1)",
+                "SELECT * FROM a LEFT JOIN b ON b.id = a.id LEFT JOIN c ON c.id = b.id RIGHT JOIN d ON d.id = a.id "
+                        + "WHERE ((a.id IS NULL) AND (d.id IS NOT NULL)) AND (d.length > 1)",
+                "SELECT * FROM a LEFT JOIN b ON b.id = a.id LEFT JOIN c ON c.id = b.id RIGHT JOIN d ON d.id = a.id "
+                        + "WHERE ((a.id IS NULL) AND (d.id IS NULL)) AND (d.length > 1)");
+    }
+
+    @Test
+    public void test() {
+        containsAtLeast("SELECT * FROM a INNER JOIN b ON b.id = a.id INNER JOIN c ON c.id = b.id AND c.id = a.id "
+                        + "WHERE c.length > 1 OR b.length > 1 OR a.length > 1",
+                "SELECT * FROM a LEFT JOIN b ON b.id = a.id LEFT JOIN c ON c.id = b.id AND c.id = a.id "
+                        + "WHERE ((b.id IS NULL) AND (a.id IS NOT NULL)) AND (a.length > 1)",
+                "SELECT * FROM a LEFT JOIN b ON b.id = a.id LEFT JOIN c ON c.id = b.id AND c.id = a.id "
+                        + "WHERE ((b.id IS NULL) AND (a.id IS NULL)) AND (a.length > 1)",
+                "SELECT * FROM a RIGHT JOIN b ON b.id = a.id LEFT JOIN c ON c.id = b.id AND c.id = a.id "
+                        + "WHERE ((a.id IS NULL) AND (b.id IS NOT NULL)) AND (b.length > 1)",
+                "SELECT * FROM a RIGHT JOIN b ON b.id = a.id LEFT JOIN c ON c.id = b.id AND c.id = a.id "
+                        + "WHERE ((a.id IS NULL) AND (b.id IS NULL)) AND (b.length > 1)",
+                "SELECT * FROM a INNER JOIN b ON b.id = a.id LEFT JOIN c ON c.id = b.id AND c.id = a.id "
+                        + "WHERE ((c.id IS NULL) AND (a.id IS NOT NULL) AND (b.id IS NOT NULL)) "
+                        + "AND (b.length > 1 OR a.length > 1)",
+                "SELECT * FROM a INNER JOIN b ON b.id = a.id LEFT JOIN c ON c.id = b.id AND c.id = a.id "
+                        + "WHERE ((c.id IS NULL) AND (a.id IS NULL) AND (b.id IS NULL)) "
+                        + "AND (b.length > 1 OR a.length > 1)",
+                "SELECT * FROM a LEFT JOIN b ON b.id = a.id RIGHT JOIN c ON c.id = b.id AND c.id = a.id "
+                        + "WHERE ((a.id IS NULL) AND (b.id IS NULL) AND (c.id IS NOT NULL)) AND (c.length > 1)",
+                "SELECT * FROM a LEFT JOIN b ON b.id = a.id RIGHT JOIN c ON c.id = b.id AND c.id = a.id "
+                        + "WHERE ((a.id IS NULL) AND (b.id IS NULL) AND (c.id IS NULL)) AND (c.length > 1)"
+                );
 
     }
 
