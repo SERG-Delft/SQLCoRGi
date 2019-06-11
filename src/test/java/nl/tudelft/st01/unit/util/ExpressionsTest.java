2507cf88770e00fe319b9e80f3d44f7d79d73f70
Dylan Franken <d.t.franken@student.tudelft.nl>
2019-06-11 02:15:45 +0200

    Add test for setJoinToInner

--- src/test/java/nl/tudelft/st01/unit/util/ExpressionsTest.java
+++ src/test/java/nl/tudelft/st01/unit/util/ExpressionsTest.java
@@ -1,5 +1,6 @@
 package nl.tudelft.st01.unit.util;
 
+import net.sf.jsqlparser.statement.select.Join;
 import nl.tudelft.st01.util.Expressions;
 import org.junit.jupiter.api.Test;
 
@@ -7,6 +8,7 @@ import java.lang.reflect.Constructor;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.mockito.Mockito.*;
 
 /**
  * Tests the {@link Expressions} utility class.
@@ -27,6 +29,31 @@ public class ExpressionsTest {
         Throwable thrown = catchThrowable(constructor::newInstance);
 
         assertThat(thrown).hasRootCauseInstanceOf(UnsupportedOperationException.class);
+    }
+
+    /**
+     * Tests whether {@link Expressions#setJoinToInner(Join)} sets a {@link Join} to {@code INNER}.
+     */
+    @Test
+    void testSetJoinToInner() {
+
+        Join innerJoin = new Join();
+        innerJoin.setInner(true);
+        innerJoin.setLeft(false);
+        innerJoin.setRight(false);
+        innerJoin.setOuter(false);
+        innerJoin.setSimple(false);
+        innerJoin.setNatural(false);
+        innerJoin.setSemi(false);
+        innerJoin.setCross(false);
+        innerJoin.setFull(false);
+
+        Join join = new Join();
+        join.setLeft(true);
+
+        Expressions.setJoinToInner(join);
+
+        assertThat(join).isEqualToComparingFieldByField(innerJoin);
     }
 
 }
