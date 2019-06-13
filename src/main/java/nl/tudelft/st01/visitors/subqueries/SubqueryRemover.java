8fa0bfe39892d1275fdba60c7b9fdbc9c42f6a69
Dylan Franken <d.t.franken@student.tudelft.nl>
2019-06-14 01:00:50 +0200

    Add JavaDoc

--- src/main/java/nl/tudelft/st01/visitors/subqueries/SubqueryRemover.java
+++ src/main/java/nl/tudelft/st01/visitors/subqueries/SubqueryRemover.java
@@ -1,4 +1,10 @@
 package nl.tudelft.st01.visitors.subqueries;
 
+import net.sf.jsqlparser.expression.Expression;
+
+/**
+ * This visitor removes subqueries from an {@link Expression} and repairs it afterwards.
+ */
 public class SubqueryRemover {
+
 }
