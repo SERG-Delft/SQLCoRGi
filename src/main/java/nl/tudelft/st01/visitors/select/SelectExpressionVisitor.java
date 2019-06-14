c3d0241fd507a2310003fea7b7f583e340fae933
Matthijs Halvemaan <m.j.w.halvemaan@student.tudelft.nl>
2019-06-14 11:16:41 +0200

    Reverse last commit

--- src/main/java/nl/tudelft/st01/visitors/select/SelectExpressionVisitor.java
+++ src/main/java/nl/tudelft/st01/visitors/select/SelectExpressionVisitor.java
@@ -221,7 +221,9 @@ public class SelectExpressionVisitor extends ExpressionVisitorAdapter {
 
         output.add(copy(likeExpression));
 
-        LikeExpression likeExpressionFlipped = (LikeExpression) copy(likeExpression);
+        LikeExpression likeExpressionFlipped = new LikeExpression();
+        likeExpressionFlipped.setLeftExpression(copy(likeExpression.getLeftExpression()));
+        likeExpressionFlipped.setRightExpression(copy(likeExpression.getRightExpression()));
         likeExpressionFlipped.setCaseInsensitive(likeExpression.isCaseInsensitive());
 
         if (!likeExpression.isNot()) {
