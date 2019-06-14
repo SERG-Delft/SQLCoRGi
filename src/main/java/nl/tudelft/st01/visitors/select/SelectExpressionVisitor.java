4fddbbfb2206420aaf764f5735b8fbf8c01be703
Matthijs Halvemaan <m.j.w.halvemaan@student.tudelft.nl>
2019-06-14 11:05:10 +0200

    Change initiation of flipped expression to a copy

--- src/main/java/nl/tudelft/st01/visitors/select/SelectExpressionVisitor.java
+++ src/main/java/nl/tudelft/st01/visitors/select/SelectExpressionVisitor.java
@@ -221,9 +221,7 @@ public class SelectExpressionVisitor extends ExpressionVisitorAdapter {
 
         output.add(copy(likeExpression));
 
-        LikeExpression likeExpressionFlipped = new LikeExpression();
-        likeExpressionFlipped.setLeftExpression(copy(likeExpression.getLeftExpression()));
-        likeExpressionFlipped.setRightExpression(copy(likeExpression.getRightExpression()));
+        LikeExpression likeExpressionFlipped = (LikeExpression) copy(likeExpression);
         likeExpressionFlipped.setCaseInsensitive(likeExpression.isCaseInsensitive());
 
         if (!likeExpression.isNot()) {
