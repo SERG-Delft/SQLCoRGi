0011d247306b9cd729065fe19ee7be32b3e3cb2e
Dylan Franken <dtfranken@gmail.com>
2019-07-17 16:46:47 +0200

    Remove name from schema

--- src/main/java/com/github/sergdelft/sqlcorgi/schema/Schema.java
+++ src/main/java/com/github/sergdelft/sqlcorgi/schema/Schema.java
@@ -6,21 +6,19 @@ import java.util.Map;
  * The schema data structure allows for storing the database schema in a systematic manner.
  */
 public class Schema {
-    private String name;
+
     private Map<String, Table> tables;
 
     /**
      * Creates an instance of a Schema.
-     * @param name The schema name.
      * @param tables The tables contained in the schema.
      */
-    public Schema(String name, Map<String, Table> tables) {
-        this.name = name;
+    public Schema(Map<String, Table> tables) {
         this.tables = tables;
     }
 
-    public String getName() {
-        return name;
+    public Table getTable(String name) {
+        return tables.get(name);
     }
 
     public Map<String, Table> getTables() {
