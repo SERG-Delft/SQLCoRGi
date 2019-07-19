cbf6740def9c8715577c6a3f7f46c2a56eb478ad
Rolf Piepenbrink <r.h.piepenbrink@student.tudelft.nl>
2019-07-19 19:17:07 +0200

    Add new exception

---
+++ src/main/java/com/github/sergdelft/sqlcorgi/exceptions/TypeCheckerException.java
@@ -0,0 +1,10 @@
+package com.github.sergdelft.sqlcorgi.exceptions;
+
+public class TypeCheckerException extends RuntimeException {
+    public TypeCheckerException() {
+    }
+
+    public TypeCheckerException(String message) {
+        super(message);
+    }
+}
