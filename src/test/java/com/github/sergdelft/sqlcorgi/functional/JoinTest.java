7292c0e204f9aed93837f637bd29281ef8a9ea5b
Dylan Franken <dtfranken@gmail.com>
2019-07-26 01:20:20 +0200

    Update JoinTest

--- src/test/java/com/github/sergdelft/sqlcorgi/functional/JoinTest.java
+++ src/test/java/com/github/sergdelft/sqlcorgi/functional/JoinTest.java
@@ -1,5 +1,9 @@
 package com.github.sergdelft.sqlcorgi.functional;
 
+import com.github.sergdelft.sqlcorgi.schema.Column;
+import com.github.sergdelft.sqlcorgi.schema.Schema;
+import com.github.sergdelft.sqlcorgi.schema.Table;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -15,6 +19,53 @@ import static com.github.sergdelft.sqlcorgi.AssertUtils.verify;
 @SuppressWarnings("checkstyle:multipleStringLiterals")
 class JoinTest {
 
+    private static Schema schema;
+
+    /**
+     * Creates a schema with tables 'a', 'b', 'c' and 'd'.
+     */
+    @BeforeAll
+    static void makeSchema() {
+
+        Table tableA = new Table("a");
+        tableA.addColumn(new Column("id", true, true, Column.DataType.NUM));
+        tableA.addColumn(new Column("length", true, false, Column.DataType.NUM));
+        tableA.addColumn(new Column("magic", false, false, Column.DataType.NUM));
+        tableA.addColumn(new Column("name", false, false, Column.DataType.STRING));
+
+        Table tableB = new Table("b");
+        tableB.addColumn(new Column("id", true, true, Column.DataType.NUM));
+        tableB.addColumn(new Column("length", true, false, Column.DataType.NUM));
+
+        Table tableTableA = new Table("TableA");
+        tableTableA.addColumn(new Column("CanBeNull", true, true, Column.DataType.NUM));
+        tableTableA.addColumn(new Column("CanBeNull2", true, false, Column.DataType.NUM));
+        tableTableA.addColumn(new Column("Var", true, false, Column.DataType.NUM));
+        tableTableA.addColumn(new Column("Value", true, false, Column.DataType.NUM));
+
+        Table tableTableB = new Table("TableB");
+        tableTableB.addColumn(new Column("CanBeNull", true, true, Column.DataType.NUM));
+        tableTableB.addColumn(new Column("CanBeNull2", true, false, Column.DataType.NUM));
+        tableTableB.addColumn(new Column("Var", true, false, Column.DataType.NUM));
+
+        Table tableAclRoles = new Table("acl_roles");
+        tableAclRoles.addColumn(new Column("id", true, true, Column.DataType.NUM));
+        tableAclRoles.addColumn(new Column("deleted", true, false, Column.DataType.NUM));
+
+        Table tableAclRolesUsers = new Table("acl_roles_users");
+        tableAclRolesUsers.addColumn(new Column("user_id", true, true, Column.DataType.NUM));
+        tableAclRolesUsers.addColumn(new Column("role_id", true, false, Column.DataType.NUM));
+        tableAclRolesUsers.addColumn(new Column("deleted", true, false, Column.DataType.NUM));
+
+        schema = new Schema();
+        schema.addTable(tableA);
+        schema.addTable(tableB);
+        schema.addTable(tableTableA);
+        schema.addTable(tableTableB);
+        schema.addTable(tableAclRoles);
+        schema.addTable(tableAclRolesUsers);
+    }
+
     /**
      * Parametrized test for a simple query with different join types with a single join condition which involves
      * nullable columns. All of which should result in the same expected output set.
@@ -26,7 +77,7 @@ class JoinTest {
     void testJoinsOnOneEqualityConditionWithNullableColumns(String joinType) {
         verify("SELECT * FROM TableA " + joinType + " JOIN TableB ON TableA.CanBeNull < TableB.CanBeNull",
 
-                null, "SELECT * FROM TableA INNER JOIN TableB ON TableA.CanBeNull < TableB.CanBeNull",
+                schema, "SELECT * FROM TableA INNER JOIN TableB ON TableA.CanBeNull < TableB.CanBeNull",
                 "SELECT * FROM TableA LEFT JOIN TableB ON TableA.CanBeNull < TableB.CanBeNull "
                     + "WHERE (TableB.CanBeNull IS NULL) AND (TableA.CanBeNull IS NOT NULL)",
                 "SELECT * FROM TableA LEFT JOIN TableB ON TableA.CanBeNull < TableB.CanBeNull "
@@ -50,7 +101,7 @@ class JoinTest {
         verify("SELECT * FROM TableA " + joinType + " JOIN TableB ON TableA.CanBeNull = TableB.CanBeNull "
                     + conditionType + " TableA.CanBeNull2 = TableB.CanBeNull2",
 
-                null, "SELECT * FROM TableA INNER JOIN TableB ON TableA.CanBeNull = TableB.CanBeNull "
+                schema, "SELECT * FROM TableA INNER JOIN TableB ON TableA.CanBeNull = TableB.CanBeNull "
                     + conditionType + " TableA.CanBeNull2 = TableB.CanBeNull2",
                 "SELECT * FROM TableA LEFT JOIN TableB ON TableA.CanBeNull = TableB.CanBeNull "
                     + conditionType + " TableA.CanBeNull2 = TableB.CanBeNull2 WHERE (TableB.CanBeNull IS NULL) AND "
@@ -78,7 +129,7 @@ class JoinTest {
     void testOnConditionsInJoins(String on) {
         verify("SELECT * FROM TableA INNER JOIN TableB ON TableA.CanBeNull " + on + " TableB.CanBeNull",
 
-                null, "SELECT * FROM TableA INNER JOIN TableB ON TableA.CanBeNull " + on + " TableB.CanBeNull",
+                schema, "SELECT * FROM TableA INNER JOIN TableB ON TableA.CanBeNull " + on + " TableB.CanBeNull",
                 "SELECT * FROM TableA LEFT JOIN TableB ON TableA.CanBeNull " + on + " TableB.CanBeNull"
                     + " WHERE (TableB.CanBeNull IS NULL) AND (TableA.CanBeNull IS NOT NULL)",
                 "SELECT * FROM TableA LEFT JOIN TableB ON TableA.CanBeNull " + on + " TableB.CanBeNull"
@@ -97,7 +148,7 @@ class JoinTest {
     void testJoinOnConditionFromSingleTableLeftComparison() {
         verify("SELECT * FROM TableA INNER JOIN TableB ON TableA.CanBeNull > 5",
 
-                null, "SELECT * FROM TableA INNER JOIN TableB ON TableA.CanBeNull > 5",
+                schema, "SELECT * FROM TableA INNER JOIN TableB ON TableA.CanBeNull > 5",
                 "SELECT * FROM TableA LEFT JOIN TableB ON TableA.CanBeNull > 5 WHERE (NOT (TableA.CanBeNull > 5))"
                     + " AND (TableA.CanBeNull IS NOT NULL)");
     }
@@ -110,7 +161,7 @@ class JoinTest {
     void testJoinOnConditionFromSingleTableRightNullable() {
         verify("SELECT * FROM TableA INNER JOIN TableB ON TableB.CanBeNull IS NULL",
 
-                null, "SELECT * FROM TableA RIGHT JOIN TableB ON TableB.CanBeNull IS NULL WHERE"
+                schema, "SELECT * FROM TableA RIGHT JOIN TableB ON TableB.CanBeNull IS NULL WHERE"
                     + " (NOT (TableB.CanBeNull IS NULL)) AND (TableB.CanBeNull IS NOT NULL)");
     }
 
@@ -121,7 +172,7 @@ class JoinTest {
     @Test
     void testJoinOnConditionFromSingleTableRightComparison() {
         verify("SELECT * FROM TableA INNER JOIN TableB ON TableB.CanBeNull > 5",
-            null, "SELECT * FROM TableA RIGHT JOIN TableB ON TableB.CanBeNull > 5 WHERE (NOT (TableB.CanBeNull > 5))"
+            schema, "SELECT * FROM TableA RIGHT JOIN TableB ON TableB.CanBeNull > 5 WHERE (NOT (TableB.CanBeNull > 5))"
             + " AND (TableB.CanBeNull IS NOT NULL)");
     }
 
@@ -136,7 +187,7 @@ class JoinTest {
         verify("SELECT * FROM TableA " + joinType + " JOIN TableB ON TableA.Var = TableB.Var "
                         + "WHERE TableA.Value > 1",
 
-                null, "SELECT * FROM TableA INNER JOIN TableB ON TableA.Var = TableB.Var WHERE TableA.Value = 2",
+                schema, "SELECT * FROM TableA INNER JOIN TableB ON TableA.Var = TableB.Var WHERE TableA.Value = 2",
                 "SELECT * FROM TableA INNER JOIN TableB ON TableA.Var = TableB.Var WHERE TableA.Value = 1",
                 "SELECT * FROM TableA INNER JOIN TableB ON TableA.Var = TableB.Var WHERE TableA.Value = 0",
                 "SELECT * FROM TableA INNER JOIN TableB ON TableA.Var = TableB.Var WHERE TableA.Value IS NULL",
@@ -162,12 +213,11 @@ class JoinTest {
     void testJoinWithWhereColumnsExcludedIfSideIsNull(String where) {
         containsAtLeast("SELECT * FROM a INNER JOIN b ON a.id = b.id OR a.length < b.length WHERE " + where,
 
-                "SELECT * FROM a INNER JOIN b ON a.id = b.id OR a.length < b.length WHERE (" + where + ")",
+                schema, "SELECT * FROM a INNER JOIN b ON a.id = b.id OR a.length < b.length WHERE (" + where + ")",
                 "SELECT * FROM a LEFT JOIN b ON a.id = b.id OR a.length < b.length "
                     + "WHERE (b.id IS NULL) AND (b.length IS NULL) AND (a.id IS NULL) AND (a.length IS NULL)",
                 "SELECT * FROM a LEFT JOIN b ON a.id = b.id OR a.length < b.length "
-                    + "WHERE (b.id IS NULL) AND (b.length IS NULL) "
-                    + "AND (a.id IS NOT NULL) AND (a.length IS NOT NULL)",
+                    + "WHERE (b.id IS NULL) AND (b.length IS NULL) AND (a.id IS NOT NULL) AND (a.length IS NOT NULL)",
                 "SELECT * FROM a RIGHT JOIN b ON a.id = b.id OR a.length < b.length "
                     + "WHERE (a.id IS NULL) AND (a.length IS NULL) AND (b.id IS NOT NULL) "
                     + "AND (b.length IS NOT NULL)",
@@ -185,7 +235,7 @@ class JoinTest {
         containsAtLeast(
             "SELECT * FROM a INNER JOIN b ON b.id = a.id WHERE a.length < 60 AND b.id > 40",
 
-            "SELECT * FROM a LEFT JOIN b ON b.id = a.id "
+            schema, "SELECT * FROM a LEFT JOIN b ON b.id = a.id "
                 + "WHERE ((b.id IS NULL) AND (a.id IS NOT NULL)) AND (a.length < 60)",
             "SELECT * FROM a LEFT JOIN b ON b.id = a.id WHERE ((b.id IS NULL) AND (a.id IS NULL)) AND (a.length < 60)",
             "SELECT * FROM a RIGHT JOIN b ON b.id = a.id WHERE ((a.id IS NULL) AND (b.id IS NOT NULL)) AND (b.id > 40)",
@@ -203,7 +253,7 @@ class JoinTest {
         containsAtLeast(
                 "SELECT * FROM a INNER JOIN b ON b.id = a.id WHERE a.name LIKE 'a%'",
 
-                "SELECT * FROM a INNER JOIN b ON b.id = a.id WHERE (a.name LIKE 'a%')",
+                schema, "SELECT * FROM a INNER JOIN b ON b.id = a.id WHERE (a.name LIKE 'a%')",
                 "SELECT * FROM a LEFT JOIN b ON b.id = a.id "
                     + "WHERE ((b.id IS NULL) AND (a.id IS NOT NULL)) AND (a.name LIKE 'a%')",
                 "SELECT * FROM a LEFT JOIN b ON b.id = a.id "
@@ -221,7 +271,7 @@ class JoinTest {
         containsAtLeast(
                 "SELECT * FROM a RIGHT JOIN b ON b.id = a.id WHERE (a.length IS NOT NULL)",
 
-                "SELECT * FROM a INNER JOIN b ON b.id = a.id WHERE (a.length IS NOT NULL)",
+                schema, "SELECT * FROM a INNER JOIN b ON b.id = a.id WHERE (a.length IS NOT NULL)",
                 "SELECT * FROM a LEFT JOIN b ON b.id = a.id "
                     + "WHERE ((b.id IS NULL) AND (a.id IS NOT NULL)) AND (a.length IS NOT NULL)",
                 "SELECT * FROM a LEFT JOIN b ON b.id = a.id "
@@ -239,7 +289,7 @@ class JoinTest {
         containsAtLeast(
                 "SELECT * FROM a RIGHT JOIN b ON a.id = b.id WHERE a.length IS NULL",
 
-                "SELECT * FROM a RIGHT JOIN b ON a.id = b.id WHERE (a.id IS NULL) AND (b.id IS NULL)",
+                schema, "SELECT * FROM a RIGHT JOIN b ON a.id = b.id WHERE (a.id IS NULL) AND (b.id IS NULL)",
                 "SELECT * FROM a RIGHT JOIN b ON a.id = b.id WHERE (a.id IS NULL) AND (b.id IS NOT NULL)",
                 "SELECT * FROM a LEFT JOIN b ON a.id = b.id "
                         + "WHERE ((b.id IS NULL) AND (a.id IS NULL)) AND (a.length IS NULL)",
@@ -257,7 +307,7 @@ class JoinTest {
         containsAtLeast(
                 "SELECT * FROM a RIGHT JOIN b ON a.id = b.id WHERE a.id IS NULL",
 
-                "SELECT * FROM a RIGHT JOIN b ON a.id = b.id WHERE (a.id IS NULL) AND (b.id IS NULL)",
+                schema, "SELECT * FROM a RIGHT JOIN b ON a.id = b.id WHERE (a.id IS NULL) AND (b.id IS NULL)",
                 "SELECT * FROM a RIGHT JOIN b ON a.id = b.id WHERE (a.id IS NULL) AND (b.id IS NOT NULL)",
                 "SELECT * FROM a LEFT JOIN b ON a.id = b.id WHERE (b.id IS NULL) AND (a.id IS NULL)",
                 "SELECT * FROM a LEFT JOIN b ON a.id = b.id WHERE (b.id IS NULL) AND (a.id IS NOT NULL)",
@@ -273,7 +323,7 @@ class JoinTest {
         containsAtLeast(
                 "SELECT * FROM a RIGHT JOIN b ON b.id = a.id WHERE b.length IN (1, 3, 4)",
 
-                "SELECT * FROM a LEFT JOIN b ON b.id = a.id WHERE (b.id IS NULL) AND (a.id IS NOT NULL)",
+                schema, "SELECT * FROM a LEFT JOIN b ON b.id = a.id WHERE (b.id IS NULL) AND (a.id IS NOT NULL)",
                 "SELECT * FROM a LEFT JOIN b ON b.id = a.id WHERE (b.id IS NULL) AND (a.id IS NULL)",
                 "SELECT * FROM a RIGHT JOIN b ON b.id = a.id "
                     + "WHERE ((a.id IS NULL) AND (b.id IS NOT NULL)) AND (b.length IN (1, 3, 4))",
@@ -290,7 +340,7 @@ class JoinTest {
         containsAtLeast(
                 "SELECT * FROM a RIGHT JOIN b ON b.id = a.id WHERE a.id = 10 AND a.length = 30",
 
-                "SELECT * FROM a INNER JOIN b ON b.id = a.id WHERE (a.id = 10) AND (a.length = 30)",
+                schema, "SELECT * FROM a INNER JOIN b ON b.id = a.id WHERE (a.id = 10) AND (a.length = 30)",
                 "SELECT * FROM a LEFT JOIN b ON b.id = a.id "
                     + "WHERE ((b.id IS NULL) AND (a.id IS NOT NULL)) AND (a.id = 10 AND a.length = 30)",
                 "SELECT * FROM a LEFT JOIN b ON b.id = a.id "
@@ -308,7 +358,7 @@ class JoinTest {
         containsAtLeast(
                 "SELECT * FROM a RIGHT JOIN b ON b.id = a.id WHERE a.length BETWEEN 10 AND 40",
 
-                "SELECT * FROM a INNER JOIN b ON b.id = a.id WHERE (a.length BETWEEN 10 AND 40)",
+                schema, "SELECT * FROM a INNER JOIN b ON b.id = a.id WHERE (a.length BETWEEN 10 AND 40)",
                 "SELECT * FROM a LEFT JOIN b ON b.id = a.id "
                     + "WHERE ((b.id IS NULL) AND (a.id IS NOT NULL)) AND (a.length BETWEEN 10 AND 40)",
                 "SELECT * FROM a LEFT JOIN b ON b.id = a.id "
@@ -337,7 +387,7 @@ class JoinTest {
                     + "AND acl_roles_users.role_id = acl_roles.id AND acl_roles_users.deleted ='0'WHERE acl_roles"
                     + ".deleted='0'",
 
-                "SELECT acl_roles.* FROM acl_roles INNER JOIN acl_roles_users ON acl_roles_users.user_id = '1'"
+                schema, "SELECT acl_roles.* FROM acl_roles INNER JOIN acl_roles_users ON acl_roles_users.user_id = '1'"
                     + " AND acl_roles_users.role_id = acl_roles.id AND acl_roles_users.deleted = '0' WHERE "
                     + "acl_roles.deleted = '0'",
                 "SELECT acl_roles.* FROM acl_roles INNER JOIN acl_roles_users ON acl_roles_users.user_id = '1' AND "
